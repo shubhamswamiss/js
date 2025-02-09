3c50f4753726b54902ac7f69b8d2a9c946cf48db
shubhamswamiss <shubhamswamiss333@gmail.com>
2025-02-09 14:21:19 +0530

    data types

---
+++ Basics/dataType.js
@@ -0,0 +1,18 @@
+// Primitive Data Type
+
+// number => range of number is 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+
+// Non-Primitive Data Types
+
+// Object
+// Array
+// function
+
+console.log(typeof null);
+console.log(typeof undefined);
