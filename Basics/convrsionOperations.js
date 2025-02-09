3106cc7d9b51e300a981a32681f00b70b1d647ef
shubhamswamiss <shubhamswamiss333@gmail.com>
2025-02-09 14:53:11 +0530

    conversion of dataTypes

---
+++ Basics/convrsionOperations.js
@@ -0,0 +1,27 @@
+let score = "shubham"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let loggedIn = "shubham"
+let booleanIsLoggedIn = Boolean(loggedIn);
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// " " => true
+// "shubham" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
