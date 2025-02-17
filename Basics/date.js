20e040a0ad82393a552e372f0057aaf57b2dff75
shubhamswamiss <shubhamswamiss333@gmail.com>
2025-02-17 19:49:27 +0530

    date

---
+++ Basics/date.js
@@ -0,0 +1,13 @@
+const currentDate = new Date();
+console.log(currentDate);
+console.log(currentDate.toDateString());
+console.log(currentDate.toISOString());
+console.log(currentDate.toJSON());
+console.log(currentDate.toLocaleDateString());
+console.log(currentDate.toLocaleString());
+
+
+
+
+
+
--- Basics/math.js
+++ Basics/math.js
@@ -7,5 +7,14 @@ const min = 1;
 const max = 6;
 
 console.log(Math.floor(Math.random() * (max-min +1) + min));
+let myCreatedDate = new Date("2023-01-14");
+console.log(myCreatedDate.toLocaleString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
+console.log(Math.floor(Date.now()/1000));
+
+
+
 
 
