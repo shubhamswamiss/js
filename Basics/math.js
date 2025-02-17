b7eddecfeb3ff3ec47b52e45ce5735dd6675158f
shubham.swami <shubham.swami@epps-erp.in>
2025-02-17 19:23:01 +0530

    math.random

---
+++ Basics/math.js
@@ -0,0 +1,11 @@
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max-min +1) + min));
+
+
